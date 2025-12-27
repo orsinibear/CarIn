ab0c3f6747c1dd6766d09cb091e7cb93e58105a1
dev.uche <uchennahanson@gmail.com>
2025-12-27 03:15:05 +0100

    feat: add Header and Footer components

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
